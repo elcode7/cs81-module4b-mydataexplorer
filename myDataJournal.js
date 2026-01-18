b7ce61b748a5f347c4441cf103497290f6833d6c
elcode7 <101850419+elcode7@users.noreply.github.com>
2026-01-17 19:30:39 -0800

    Created my weekly journal
    Added a weekly data journal module with predictions and data for each day.

--- myDataJournal.js
+++ myDataJournal.js
@@ -1,1 +1,68 @@
+/*
+MODULE 4 – Weekly Data Journal
+
+PREDICTIONS (before coding):
+- The day with the most screen time will likely be a weekday (probably Wednesday).
+- Best focus day will be a day with more sleep and less screen time.
+- More caffeine might help focus a little, but too much probably doesn’t.
+*/
+
+const weekData = [
+  {
+    day: "Monday",
+    sleepHours: 6,
+    screenTime: 8,
+    mood: "tired",
+    caffeineIntake: 2,
+    focusLevel: 6
+  },
+  {
+    day: "Tuesday",
+    sleepHours: 7,
+    screenTime: 7,
+    mood: "okay",
+    caffeineIntake: 2,
+    focusLevel: 7
+  },
+  {
+    day: "Wednesday",
+    sleepHours: 5,
+    screenTime: 9,
+    mood: "stressed",
+    caffeineIntake: 3,
+    focusLevel: 5
+  },
+  {
+    day: "Thursday",
+    sleepHours: 8,
+    screenTime: 6,
+    mood: "good",
+    caffeineIntake: 1,
+    focusLevel: 9
+  },
+  {
+    day: "Friday",
+    sleepHours: 7,
+    screenTime: 7.5,
+    mood: "happy",
+    caffeineIntake: 2,
+    focusLevel: 8
+  },
+  {
+    day: "Saturday",
+    sleepHours: 9,
+    screenTime: 5,
+    mood: "relaxed",
+    caffeineIntake: 1,
+    focusLevel: 7
+  },
+  {
+    day: "Sunday",
+    sleepHours: 8,
+    screenTime: 6,
+    mood: "calm",
+    caffeineIntake: 1,
+    focusLevel: 8
+  }
+];
 
